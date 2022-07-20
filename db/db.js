096d0742e7666f1efc1ebe175b8df423a588868d
Evgenia-bit <ev.mo4alina@yandex.ru>
2022-07-20 17:52:04 +0300

    Variable names changed

--- db/db.js
+++ db/db.js
@@ -19,7 +19,7 @@ module.exports = {
         return await Person.findOne({nickname})
     },
     getOwnDisks: async (person_id) => {
-        const result = await TakenItem.aggregate([
+        const takenItems = await TakenItem.aggregate([
             {
                 '$match': {
                     'ownerId': mongoose.Types.ObjectId(person_id)
@@ -33,16 +33,16 @@ module.exports = {
                 }
             }
         ])
-        if(!result) return null
-        return result.map(elem=> {
+        if(!takenItems) return null
+        return takenItems.map(takenItem=> {
             return {
-                id: elem.disk[0]._id,
-                name: elem.disk[0].name
+                id: takenItem.disk[0]._id,
+                name: takenItem.disk[0].name
             }
         })
     },
     getFreeDisks: async (person_id) => {
-        const result = await TakenItem.aggregate([
+        const takenItems = await TakenItem.aggregate([
             {
                 '$match': {
                     '$expr': {
@@ -63,16 +63,16 @@ module.exports = {
                 }
             }
         ])
-        if(!result) return null
-        return result.map(elem=> {
+        if(!takenItems) return null
+        return takenItems.map(takenItem=> {
             return {
-                id: elem.disk[0]._id,
-                name: elem.disk[0].name
+                id: takenItem.disk[0]._id,
+                name: takenItem.disk[0].name
             }
         })
     },
     getDisksTakenByPerson: async (person_id) => {
-        const result = await TakenItem.aggregate([
+        const takenItems = await TakenItem.aggregate([
                 {
                     '$match': {
                         'ownerId': {
@@ -88,16 +88,16 @@ module.exports = {
                     'as': 'disk'
                 }
             }])
-        if(!result) return null
-        return result.map(elem=> {
+        if(!takenItems) return null
+        return takenItems.map(takenItem=> {
             return {
-                id: elem.disk[0]._id,
-                name: elem.disk[0].name
+                id: takenItem.disk[0]._id,
+                name: takenItem.disk[0].name
             }
         })
     },
     getDisksTakenFromPerson: async (person_id) => {
-        const result = await TakenItem.aggregate([
+        const takenItems = await TakenItem.aggregate([
             {
                 '$match': {
                     'ownerId': mongoose.Types.ObjectId(person_id),
@@ -121,34 +121,33 @@ module.exports = {
                 }
             }
         ])
-        if(!result) return null
-        return result.map(elem => {
+        if(!takenItems) return null
+        return takenItems.map(takenItem => {
            return {
-                diskId: elem.disk[0]._id,
-                diskName: elem.disk[0].name,
-                personNickname: elem.person[0].nickname
+                diskId: takenItem.disk[0]._id,
+                diskName: takenItem.disk[0].name,
+                personNickname: takenItem.person[0].nickname
            }
         })
     },
     getTakenItem: async (diskId) => {
-        const result = await TakenItem.findOne({diskId: mongoose.Types.ObjectId(diskId)})
-        if(!result) return null
-        return {ownerid: result.ownerId.toString(), holderid: result.holderId.toString()}
+        const takenItem = await TakenItem.findOne({diskId: mongoose.Types.ObjectId(diskId)})
+        if(!takenItem) return null
+        return {
+            ownerid: takenItem.ownerId.toString(),
+            holderid: takenItem.holderId.toString()
+        }
     },
     giveDisk: async (diskId) => {
-       try {
-           const res = await TakenItem.updateOne({diskId: mongoose.Types.ObjectId(diskId)},
-               [
-                     {"$set": {"holderId": "$ownerId"}}
-                  ])
-           return res.acknowledged
-       } catch (e) {
-           console.log(e.message)
-       }
+        const resultsOfUpdatingTakenElement = await TakenItem.updateOne({diskId: mongoose.Types.ObjectId(diskId)},
+            [
+                   {"$set": {"holderId": "$ownerId"}}
+               ])
+        return resultsOfUpdatingTakenElement.acknowledged
     },
     takeDisk: async (person_id, diskId) => {
-        const res = await TakenItem.updateOne({diskId:  mongoose.Types.ObjectId(diskId)}, {holderId: person_id})
-        return res.acknowledged
+        const resultsOfUpdatingTakenElement = await TakenItem.updateOne({diskId:  mongoose.Types.ObjectId(diskId)}, {holderId: person_id})
+        return resultsOfUpdatingTakenElement.acknowledged
     }
 }
 
